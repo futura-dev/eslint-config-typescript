67cacb683c9bb24f7e127e9db21057a78aefcc2a
Samuel Roberto <samuel.roberto997@gmail.com>
2023-05-23 16:02:55 +0200

    feat: add prettier support

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  arrowParens: true,
+  endOfLine: "lf",
+  arrowParens: "avoid"
+};
