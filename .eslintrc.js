2a03bd676ba224866478c4c9adf480948f0ccfbe
Samuel Roberto <samuel.roberto997@gmail.com>
2023-05-23 16:20:37 +0200

    fix: changed rules and exports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
@@ -11,8 +13,6 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-empty-interface": "error",
     "@typescript-eslint/no-implicit-any-catch": "error",
-    "@typescript-eslint/no-import-type-side-effects": "error",
-    "@typescript-eslint/promise-function-async": "error",
-    "@typescript-eslint/strict-boolean-expressions": "error"
+    "@typescript-eslint/no-import-type-side-effects": "error"
   }
 };
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const eslintrc = require('./.eslintrc.js')
-const prettierrc = require('./.prettierrc.js')
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const eslintrc = require("./.eslintrc.js");
+const prettierrc = require("./.prettierrc.js");
 
 module.exports = {
   eslintrc,
-  prettierrc,
-}
+  prettierrc
+};
+
+export default eslintrc;
