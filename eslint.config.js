4e0a6bd5416cea014621a434a80e0bb92b9e6c82
faridevnz <ale.farinelli@icloud.com>
2024-10-31 15:36:59 +0100

    chore: replace no-empty-interface with no-empty-object-type

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ module.exports = [
     rules: {
       '@typescript-eslint/array-type': 'error',
       '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/no-empty-object-type': 'error',
       '@typescript-eslint/no-import-type-side-effects': 'error',
       '@typescript-eslint/explicit-function-return-type': 'error',
 
