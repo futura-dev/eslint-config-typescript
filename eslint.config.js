35017eccbc7a02889d187b368caded8fdf0abe45
faridevnz <ale.farinelli@icloud.com>
2025-01-24 22:45:27 +0100

    chore: remove import eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,10 @@
 const typescriptEslintParser = require("@typescript-eslint/parser");
 const prettierEslintConfig = require("eslint-plugin-prettier/recommended");
 const typescriptEslintConfig = require("typescript-eslint");
-const eslintImportPlugin = require("eslint-plugin-import");
 
 module.exports = [
   prettierEslintConfig,
   ...typescriptEslintConfig.configs.recommended,
-  eslintImportPlugin.flatConfigs.recommended,
   {
     files: ["**/*.ts"],
     ignores: ["dist", "build", "node_modules"],
@@ -21,7 +19,6 @@ module.exports = [
       "@typescript-eslint/no-empty-object-type": "error",
       "@typescript-eslint/no-import-type-side-effects": "error",
       "@typescript-eslint/explicit-function-return-type": "error",
-      "import/no-duplicates": "warn",
 
       "prettier/prettier": [
         "error",
